0977b2dd4c84394685a06494fadb7f941f743842
jvellojinp <30836049+jvellojinp@users.noreply.github.com>
2022-02-28 11:57:59 -0500

    Update
    This code solves an arbitrary 2nd order differential equation through 4th order Runge-Kutta/Shooting method. In this commit, we solve the Schrödinger equation with
    a linear potential for an arbitrary wavenumber, then we choose those k which satisfies the boundary condition by finding the roots of Solution(boundary) vs k through my own (multiple-)root finder code.

--- grafrec.cpp
+++ grafrec.cpp
@@ -5,7 +5,11 @@
 #include <math.h>
 #include <fstream>
 
+#define EPSILON 0.0001
+
+
 using namespace std;
+
 
 double f1(double t, double x, double v)
 {
@@ -14,7 +18,7 @@ double f1(double t, double x, double v)
 
 double f2(double t, double x, double v, double k)
 {
-  return(( 100*x-k*k)*v);
+  return( (100*pow(2.7,-(1-2*x)*(1-2*x))-k*k)*x );
 }
 
 double r(double t0, double x0, double v0, double t, double h, double k)
@@ -42,19 +46,52 @@ double r(double t0, double x0, double v0, double t, double h, double k)
   return x0;
 }
 
+// Bisection function
+double bisection(double a, double b)
+{
+  double t0 = 0, x0 = 0, v0 = 1, h = 0.001;
+  if (r(t0, x0, v0, 1, h, a)*r(t0, x0, v0, 1, h, b) >= 0)
+  {
+      cout << "You have not assumed right a and b\n";
+  }
+
+  double c;
+  while ((b-a) >= EPSILON)
+  {
+      // Find middle point
+      c = (a+b)/2;
+
+      // Check if middle point is root
+      if (r(t0, x0, v0, 1, h, c) == 0.0)
+          break;
+
+      // Decide the side to repeat the steps
+      else if (r(t0, x0, v0, 1, h, c)*r(t0, x0, v0, 1, h, a) < 0)
+          b = c;
+      else
+          a = c;
+  }
+  return c;
+}
+
 int main()
 {
-  ofstream file;
-  file.open("Disparo.txt");
-    double t0 = 0, x0 = 0, v0 = 1, h = 0.001, eps = 0.001, k = 0;
+  int root_counter = 0, root_number = 5;
+  double k = 0, k_next = 0, t0 = 0, x0 = 0, v0 = 1, h = 0.001, step = 0.001;
 
-    do
-    {
-      file << k << " " << r(t0, x0, v0, 1, h, k) << endl;
-      k = k + 0.001;
-    }
-    while(k<=20);
-  file.close();
+  do
+  {
+      //Inittialy try with step = .01 and root_counter = 1,2
+      
+      k_next = k + step;
+      if(r(t0, x0, v0, 1, h, k)*r(t0, x0, v0, 1, h, k_next) <= 0)
+      {
+          root_counter = root_counter + 1;
+          cout << "Root interval #" << root_counter << ": " << "a = " << k << ", b = " << k_next << "-> Root = " << bisection(k,k_next) <<  endl;
+      }
+      k = k_next;
+  }
+  while(k <= 20  && root_counter != root_number);
 
   return 0;
 }
